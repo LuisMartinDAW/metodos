f93c05bdff804c4d34c025bb51ad18fe060b8af7
Luisvi <94524327+LuisMartinDAW@users.noreply.github.com>
2022-05-09 08:24:20 +0000

    nombres de estudiantes

---
+++ index.js
@@ -0,0 +1,34 @@
+//sample array of students
+
+const students = [
+  {
+    name: "Jill",
+    lastname: "Doe",
+    age:24,
+    course: "Marketing",
+  },
+  {
+    name: "Jhon",
+    lastname: "Doe",
+    age: 22,
+    course: "web Development",
+  },
+  {
+    name: "Jack",
+    lastname: "Doe",
+    age: 20,
+    course: "Accounting",
+  },
+  {
+    name: "Ryan Jhon",
+    lastname: "Ray",
+    age: 23,
+    course: "web Development",
+  },
+  {
+    name: "Jane",
+    lastname: "Doe",
+    age: 20,
+    course: "Finalcial Management",
+  }
+];
