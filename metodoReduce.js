2f2892d06c374f63e5190cb0194fbc14d370dd9e
Luisvi <94524327+LuisMartinDAW@users.noreply.github.com>
2022-05-17 18:25:20 +0000

    Reduce con función anónima

---
+++ metodoReduce.js
@@ -0,0 +1,82 @@
+const students = [
+  {
+    name: "Jill",
+    lastname: "Doe",
+    age: 24,
+    course: "Marketing",
+  },
+  {
+    name: "Jhon",
+    lastname: "Doe",
+    age: 22,
+    course: "web Development",
+  },
+  {
+    name: "Jack",
+    lastname: "Doe",
+    age: 20,
+    course: "Accounting",
+  },
+  {
+    name: "Ryan Jhon",
+    lastname: "Ray",
+    age: 23,
+    course: "web Development",
+  },
+  {
+    name: "Jane",
+    lastname: "Doe",
+    age: 20,
+    course: "Finalcial Management",
+  }
+];
+
+const developers = [
+  {
+    id:1,
+    name: 'john',
+    skills:['HTML', 'React', 'Javascript', 'Java']
+  },
+    {
+    id:2,
+    name: "Jane",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "NodeJS"] 
+  },
+    {
+      id:3,
+      name: "Jack",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "NodeJS"]
+    }
+]
+
+const resul = developers.reduce(function(allSkills, student){
+  return [...allSkills, students.skills]
+}, [])
+
+console.log(resul);
+/*Queremos obtener una variable de toda la suma de la edad de los estudiantes, por ejemplo*/
+/*
+let total = 0;
+for (let i =0; i < students.length; i++){
+ total += students[i].age
+}
+console.log(total);
+*/
+
+/*
+
+const result = students.reduce((total, student) => {
+
+  return total + student.age;
+  
+}, 0);
+
+console.log(result);
+
+*/
+
+/*
+const result = students.reduce((total, student) => total + student.age, 0);
+
+console.log(result);
+*/
